43935269b1853124ca42df339b7c3b2597bcc015
Mauricio Ramirez <mau.ram.ro@gmail.com>
2016-02-12 14:54:58 -0600

    first commit

---
+++ www/js/controllers.js
@@ -0,0 +1,108 @@
+angular.module('starter.controllers', ['LocalStorageModule'])
+
+.config(['localStorageServiceProvider', function(localStorageServiceProvider){
+    localStorageServiceProvider.setPrefix('ls');
+  }])
+.controller('DashCtrl', function($scope) {
+
+  var deploy = new Ionic.Deploy();
+
+  // Update app code with new release from Ionic Deploy
+  $scope.doUpdate = function() {
+    deploy.update().then(function(res) {
+      console.log('Ionic Deploy: Update Success! ', res);
+    }, function(err) {
+      console.log('Ionic Deploy: Update error! ', err);
+    }, function(prog) {
+      console.log('Ionic Deploy: Progress... ', prog);
+    });
+  };
+
+  // Check Ionic Deploy for new code
+  $scope.checkForUpdates = function() {
+    console.log('Ionic Deploy: Checking for updates');
+    deploy.check().then(function(hasUpdate) {
+      console.log('Ionic Deploy: Update available: ' + hasUpdate);
+      $scope.hasUpdate = hasUpdate;
+    }, function(err) {
+      console.error('Ionic Deploy: Unable to check for updates', err);
+    });
+  }
+})
+
+.controller('ChatsCtrl', function($scope, Chats, $ionicModal, localStorageService) {
+  // With the new view caching in Ionic, Controllers are only called
+  // when they are recreated or on app start, instead of every page change.
+  // To listen for when this page is active (for example, to refresh data),
+  // listen for the $ionicView.enter event:
+  //
+  //$scope.$on('$ionicView.enter', function(e) {
+  //});
+
+  var taskData = 'task';
+
+  //initialize the tasks scope with empty array
+  $scope.tasks = [];
+
+  //initialize the task scope with empty object
+  $scope.task = [];
+
+  $scope.getTasks = function () {
+      //fetches task from local storage
+      if (localStorageService.get(taskData)) {
+          $scope.tasks = localStorageService.get(taskData);
+      } else {
+          $scope.tasks = [];
+      }
+  };
+
+   $scope.createTask = function () {
+      //creates a new task
+      $scope.tasks.push($scope.task);
+      localStorageService.set(taskData, $scope.tasks);
+      $scope.task = {};
+
+      //close new task modal
+      $scope.modal.hide();
+  };
+
+  $scope.chats = Chats.all();
+  $scope.remove = function(chat) {
+    Chats.remove(chat);
+  };
+
+  $ionicModal.fromTemplateUrl('templates/form-modal.html', {
+    scope: $scope,
+    animation: 'slide-in-up'
+  }).then(function(modal) {
+    $scope.modal = modal;
+  });
+  $scope.openModal = function() {
+    $scope.modal.show();
+  };
+  $scope.closeModal = function() {
+    $scope.modal.hide();
+  };
+  //Cleanup the modal when we're done with it!
+  $scope.$on('$destroy', function() {
+    $scope.modal.remove();
+  });
+  // Execute action on hide modal
+  $scope.$on('modal.hidden', function() {
+    // Execute action
+  });
+  // Execute action on remove modal
+  $scope.$on('modal.removed', function() {
+    // Execute action
+  });
+})
+
+.controller('ChatDetailCtrl', function($scope, $stateParams, Chats) {
+  $scope.chat = Chats.get($stateParams.chatId);
+})
+
+.controller('AccountCtrl', function($scope) {
+  $scope.settings = {
+    enableFriends: true
+  };
+});
